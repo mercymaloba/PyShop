c5748b5e369ab612386cb37e234d552f50e15642
malobs <malobamercy958@gmail.com>
2023-09-19 22:11:07 +0300

    creating index page

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Product, Offer
 
 class OfferAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock')
+    list_display = ('code', 'description', 'discount')
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock')
